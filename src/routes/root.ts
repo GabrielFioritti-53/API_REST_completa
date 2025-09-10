3fa7f4c73f77497c691698d7019b672984b34bd7
Paula Tomas <paulatomas1901@gmail.com>
2025-09-10 11:14:35 -0300

    get para isAdmin

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,11 +1,13 @@
 import type { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
 import { Type } from "@fastify/type-provider-typebox";
+import { Usuario } from "./usuarios";
+import { usuarios } from "../plugins/basedatos";
 
 export const roots: FastifyPluginAsyncTypebox = async function (
   fastify,
   options: object
 ) {
-  fastify.get(
+/*   fastify.get(
     "/",
     {
       schema: {
@@ -31,6 +33,40 @@ export const roots: FastifyPluginAsyncTypebox = async function (
     },
     async (request, reply) => {
       return { ping: "ok" };
+    }
+  ); */
+
+fastify.get(
+    "/", //en vez de params ahora usa querystring *1
+    {
+      schema: {
+        summary: "Obtener todos los usuarios",
+        description: "Retorna la lista de usuarios",
+        tags: ["listaUsuarios"],
+        querystring: Type.Object({
+          nombre: Type.Optional(Type.String({ minLength: 2 })),
+        }),
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              ping: { type: "string" },
+            },
+          },
+          401: {
+            type: "object",
+            properties: {
+              error: { type: "string" },
+            },
+          },
+        },
+      },
+    },
+    async function (request) {
+      const query = request.query as { isAdmin: boolean };
+      if (query.isAdmin) {
+        return usuarios;
+      }
     }
   );
 };
